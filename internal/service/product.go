68bf5e18c5844af28121cd23f7de17d058fff889
bin <2140354088@qq.com>
2025-11-22 00:37:49 +0800

    改进redis处理, 解决三个最常见的缓存问题

--- internal/service/product.go
+++ internal/service/product.go
@@ -6,13 +6,24 @@ import (
 	"SneakerFlash/internal/repository"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"math/rand"
 	"time"
+
+	"golang.org/x/sync/singleflight"
+	"gorm.io/gorm"
 )
 
 type ProductService struct {
 	repo *repository.ProductRepo
+	// 归并重复请求, 防止缓存击穿
+	sf singleflight.Group
 }
+
+var (
+	ErrProductNotFound = errors.New("找不到商品信息")
+)
 
 func NewProductService(repo *repository.ProductRepo) *ProductService {
 	return &ProductService{
@@ -53,6 +64,11 @@ func (s *ProductService) GetProductByID(id uint) (*model.Product, error) {
 	val, err := redis.RDB.Get(ctx, cacheKey).Result()
 	if err == nil {
 		// 命中缓存了
+		// 缓存穿透的基本解法, 不存在的商品我们存储 null 空标识符 然后如果val 是null 就直接返回错误, 不给请求访问数据库
+		if val == "null" {
+			return nil, ErrProductNotFound
+		}
+
 		var p model.Product
 		if json.Unmarshal([]byte(val), &p) == nil {
 			return &p, nil
@@ -60,14 +76,43 @@ func (s *ProductService) GetProductByID(id uint) (*model.Product, error) {
 	}
 
 	// 缓存未命中, 查数据库
-	p, err := s.repo.GetByID(id)
+	// 用 singleflight 解决缓存击穿, 阻塞重复请求
+	raw, err, _ := s.sf.Do(cacheKey, func() (interface{}, error) {
+		// 先再查一次redis, 可能会有别的请求正好查完库写入redis
+		if val, err := redis.RDB.Get(ctx, cacheKey).Result(); err == nil {
+			if val == "null" {
+				return nil, ErrProductNotFound
+			}
+			var p model.Product
+			json.Unmarshal([]byte(val), &p)
+			return p, nil
+		}
+
+		// 查数据库
+		p, err := s.repo.GetByID(id)
+		if err != nil {
+			// 查不到, 设置 null
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				redis.RDB.Set(ctx, cacheKey, "null", time.Minute*5)
+				return nil, err
+			}
+
+			// 查数据库出错了, 难以定性, 直接返回 err
+			return nil, err
+		}
+
+		// 将数据库的数据写回 redis
+		// 我们要处理缓存雪崩, 最普遍的方法是设置随机过期时间
+		expriation := time.Hour + time.Duration(rand.Intn(1800)*int(time.Second))
+
+		data, _ := json.Marshal(p)
+		redis.RDB.Set(ctx, cacheKey, data, expriation)
+		return p, nil
+	})
+
 	if err != nil {
 		return nil, err
 	}
 
-	// 写回 redis
-	data, _ := json.Marshal(p)
-	redis.RDB.Set(ctx, cacheKey, data, time.Hour)
-
-	return p, nil
+	return raw.(*model.Product), nil
 }
