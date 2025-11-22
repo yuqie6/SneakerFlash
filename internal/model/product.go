6456145dcb2d0eedcf5ad95e6120ecb39bac3bb7
bin <2140354088@qq.com>
2025-11-22 21:18:18 +0800

    为商品指定唯一索引

--- internal/model/product.go
+++ internal/model/product.go
@@ -8,7 +8,7 @@ import (
 
 type Product struct {
 	gorm.Model
-	Name      string    `gorm:"type:varchar(100);not null" json:"name"`
+	Name      string    `gorm:"type:varchar(100);not null;uniqueIndex:idx_product_name" json:"name"`
 	Price     float64   `gorm:"type:decimal(10,2);not null" json:"price"`
 	Stock     int       `gorm:"not null" json:"stock"`
 	StartTime time.Time `gorm:"not null" json:"start_time"`
