641205355744031e7b0a566be89d37277f5f0004
bin <2140354088@qq.com>
2025-11-26 19:30:20 +0800

    合并配置

--- internal/config/config.go
+++ internal/config/config.go
@@ -69,6 +69,21 @@ type LoggerConfig struct {
 	MaxSize    int    `mapstructure:"max_size"`
 }
 
+type RiskConfig struct {
+	// 接口级限流
+	LoginRate    RateLimitConfig `mapstructure:"login_rate"`
+	SeckillRate  RateLimitConfig `mapstructure:"seckill_rate"`
+	PayRate      RateLimitConfig `mapstructure:"pay_rate"`
+	ProductRate  RateLimitConfig `mapstructure:"product_rate"`
+	Enable       bool            `mapstructure:"enable"`
+	HotspotBurst int             `mapstructure:"hotspot_burst"` // 默认热点 burst
+}
+
+type RateLimitConfig struct {
+	Rate  int `mapstructure:"rate"`  // 每秒令牌
+	Burst int `mapstructure:"burst"` // 桶大小
+}
+
 var Conf Config
 
 func Init() {
